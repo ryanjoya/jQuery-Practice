33544317a663526a164cc271a36504ec87ff128f
Ryan Joya <ryanjoya@users.noreply.github.com>
2014-12-01 19:20:46 -0600

    Create Remove What's Been Clicked.js

---
+++ Remove What's Been Clicked.js
@@ -0,0 +1,11 @@
+$(document).ready(function ()
+    {$("#button").click(function ()
+        {var toAdd = $("input[name=checklistItem]".val();
+        });
+    });
+
+$(document).ready(function ()
+    {
+    $("" + toAdd + "").append(".list");
+    }
+);
